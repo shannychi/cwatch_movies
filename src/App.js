098e8cd172de5ff27ba58351e627e731a0c1da3f
Edeh sharon <Pearllizzy196@gmail.com>
2023-09-12 22:16:32 +0100

    fetch Api

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React, { useEffect, useState} from 'react';
 import './App.css';
 import MovieBox from './MovieBox';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Navbar } from 'react-bootstrap';
 
 const API_URL="https://api.themoviedb.org/3/movie/popular?api_key=93f2cc51175884f5031b4118682aa465";
+const API_IMG="https://image.tmdb.org/t/p/w500/"
 
 function App() {
    const [movies, setMovies]=useState([]);
@@ -17,11 +19,40 @@ useEffect(() => {
     })
 }, [])
 
+
+
+
  return (
-  <div className='App'>
+  <>
+ 
+ <header>
+ <div
+        id='intro-example'
+        className='p-5 text-center bg-image'
+        style={{height:'600px',width:'100%', backgroundImage: "url('')" }}
+      >
+        <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.7)' }}>
+          <div className='d-flex justify-content-center align-items-center h-100'>
+            <div className='text-white'>
+              <h1 className='mb-3'></h1>
+              <h5 className='mb-4'></h5>
+            
+              
+            </div>
+          </div>
+        </div>
+      </div>
+ </header>
+
+
+<div className='container'>
+    <div className='grid'>
     {movies.map((moviesReq)=>
     <MovieBox key={moviesReq.id} {...moviesReq}/>)}
   </div>
+  </div>
+  </>
+  
  );
  }
 export default App;
--- src/MovieBox.js
+++ src/MovieBox.js
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
+import { Modal } from 'react-bootstrap';
 
 const API_IMG="https://image.tmdb.org/t/p/w500/"
 
 const MovieBox =({title, poster_path, vote_average, release_date, runtime, overview}) => {
+
+    const [show, setshow]=useState(false);
+
+    const handleshow=()=>setshow(true);
+    const handleClose=()=>setshow(false);
     return(
-        <div>
-         <h1>{title}</h1>
-         <img src={API_IMG+poster_path}/>
-         <p>{overview}</p>
+        <div className="card text-center bg-light mb-3">
+            <div className="card-body">
+              <img className="card-img-top" style={{width:'100%'}} src={API_IMG+poster_path}/>
+              <div className="card-body">
+                  <button type="button" className="btn btn-dark" onClick={handleshow}>view more</button>
+                  <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title></Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                    <img className="card-img-top" src={API_IMG+poster_path}/>
+                    <h3>{title}</h3>
+                    <h4>ImDb: {vote_average}</h4>
+                    <h5>Release Date: {release_date}</h5>
+                    <br></br>
+                    <h6>Overview</h6>
+                    <p>{overview}</p>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button varient="secondary" onClick={handleClose}>close</Button>
+                    </Modal.Footer>
+                  </Modal>
+              </div>
+            </div>
+        
         </div>
     )
 }
