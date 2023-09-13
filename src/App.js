d62e430b4e66b352b45ca65c92cdc0f923279539
Edeh sharon <Pearllizzy196@gmail.com>
2023-09-13 07:10:35 +0100

    added form

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState} from 'react';
 import './App.css';
 import MovieBox from './MovieBox';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Navbar } from 'react-bootstrap';
+import { Container, Form, FormControl, Navbar } from 'react-bootstrap';
 
 const API_URL="https://api.themoviedb.org/3/movie/popular?api_key=93f2cc51175884f5031b4118682aa465";
 const API_IMG="https://image.tmdb.org/t/p/w500/"
@@ -25,24 +25,7 @@ useEffect(() => {
  return (
   <>
  
- <header>
- <div
-        id='intro-example'
-        className='p-5 text-center bg-image'
-        style={{height:'600px',width:'100%', backgroundImage: "url('')" }}
-      >
-        <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.7)' }}>
-          <div className='d-flex justify-content-center align-items-center h-100'>
-            <div className='text-white'>
-              <h1 className='mb-3'></h1>
-              <h5 className='mb-4'></h5>
-            
-              
-            </div>
-          </div>
-        </div>
-      </div>
- </header>
+
 
 
 <div className='container'>
